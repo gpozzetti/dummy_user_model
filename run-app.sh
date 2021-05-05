ad866d378098b39beaf5178d9297c7ad5f367a39
cedric.renzi <cedric.renzi@ittm-solutions.com>
2021-05-05 23:58:11 +0200

    adding the helper scripts, some config files (git, rsync), requirements, tests init setup

---
+++ run-app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Some colors ...
+PUR='\033[0;35m'
+BLU='\033[0;34m'
+GRE='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${BLU}Run application${NC}"
+echo "UPGRADE to force flask to flush and start new db connection in case something went wrong"
+../env/bin/python3 -m flask db upgrade
+
+../env/bin/python3 plain_version.py
